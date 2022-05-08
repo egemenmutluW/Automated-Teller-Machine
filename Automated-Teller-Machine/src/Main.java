b56aee925509a8ed2473ea443ac92a53cee53373
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-08 15:58:43 +0300

    add(Main): Added withdrawal, deposit and balance viewing features

--- Automated-Teller-Machine/src/Main.java
+++ Automated-Teller-Machine/src/Main.java
@@ -1,5 +1,26 @@
+import GUI.Gui;
+import DataManager.Manager;
+import Model.Customer;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String e = "n";
+        Scanner r = new Scanner(System.in);
+        Customer c1 = new Customer();
+        c1.setBalance(0);
+        c1.setName("test");
+        c1.setSurName("test");
+        do{
+            Gui gui = new Gui();
+            Manager manager = new Manager();
+            String choiceMenu;
+
+            choiceMenu = gui.menuStart();
+            c1 = manager.selectionMenu(choiceMenu,c1);
+            gui.exit();
+            e = r.nextLine();
+        }while (e.equals("n"));
     }
 }
