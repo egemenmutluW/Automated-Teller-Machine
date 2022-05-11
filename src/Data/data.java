e49c52a093ab74d2b70060dc87b03cf63b85cda0
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-11 12:54:42 +0300

    add(Data): static list

---
+++ src/Data/data.java
@@ -0,0 +1,10 @@
+package Data;
+
+import Model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class data {
+    public static List<Customer> customerList = new ArrayList<Customer>();
+}
