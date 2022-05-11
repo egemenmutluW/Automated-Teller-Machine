bc8d3f3385fa80c401393c57339ad8e942cbd6db
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-11 12:55:00 +0300

    add(Datamanager): Customer manager

--- src/DataManager/CustomerManager.java
+++ src/DataManager/CustomerManager.java
@@ -1,2 +1,35 @@
-package DataManager;public class CustomerManager {
+package DataManager;
+
+import Data.data;
+import Model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerManager {
+    public static boolean loginControl(String name, String surname){
+        boolean control = false;
+
+        for (Customer c:data.customerList) {
+            if (c.getName().equals(name) && c.getSurName().equals(surname) ){
+                System.out.println("Hello " + name);
+                control = true;
+                break;
+            }
+        }
+        return control;
+    }
+
+    public static List<String> getUserData(String name, String surname){
+        List<String> user = new ArrayList<String>();
+
+        for (Customer c :data.customerList) {
+            if (c.getName().equals(name) && c.getSurName().equals(surname) ){
+                user.add(name);
+                user.add(c.getName());
+                user.add(String.valueOf(c.getBalance()));
+            }
+        }
+        return user;
+    }
 }
